46f73284b1fd2770b456f86280379fd9797185b0
vsoch <vsoch@users.noreply.github.com>
2022-03-11 15:42:13 -0700

    refactoring online-ml into a class with functions
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- 2.online-ml.py
+++ 2.online-ml.py
@@ -32,99 +32,166 @@ def get_parser():
     return parser
 
 
-def iter_sentences(errors, return_text=False, return_raw=False):
-    for i, entry in enumerate(errors):
-        print("%s of %s" % (i, len(errors)), end="\r")
-        # Pre, text, and post
-        raw = entry.get("text")
-        if not raw:
-            continue
+class ModelBuilder:
+    def __init__(self, errors, host="http://127.0.0.1:8000", prefix=None, datadir=None):
+        self.errors = errors
+        self.datadir = datadir
+        self.init_client(host, prefix)
 
-        # Split based on error
-        if "error:" not in raw:
-            continue
-
-        text = raw.split("error:", 1)[-1]
-        if return_text:
-            yield text
-        elif return_raw:
-            yield raw
+    def init_client(self, host, prefix=None):
+        """
+        Create a river client (this is for development server, not yet spack monitor)
+        """
+        if prefix:
+            self.cli = Client(host, prefix=prefix)
         else:
-            tokens = process_text(text)
-            sentence = " ".join(tokens)
-            if not tokens or not sentence.strip():
+            self.cli = Client(host)
+
+    def iter_sentences(self, return_text=False, return_raw=False):
+        """
+        Yield sentences (parsed) to learn from.
+        """
+        for i, entry in enumerate(self.errors):
+            print("%s of %s" % (i, len(self.errors)), end="\r")
+
+            # Pre, text, and post
+            raw = entry.get("text")
+            if not raw:
                 continue
-            yield sentence
 
+            # Split based on error
+            if "error:" not in raw:
+                continue
 
-def build_dbstream(cli, errors, model_name, datadir):
-    """
-    Build the dbstream model with a particular name.
-    https://riverml.xyz/latest/api/cluster/DBSTREAM
-    """
-    exists = True
-    if model_name not in cli.models()["models"]:
-        model = feature_extraction.BagOfWords() | cluster.DBSTREAM(
-            clustering_threshold=1.5,
-            fading_factor=0.05,
-            cleanup_interval=4,
-            intersection_factor=0.5,
-            minimum_weight=1,
-        )
-        model_name = cli.upload_model(model, "cluster", model_name=model_name)
-        exists = False
+            text = raw.split("error:", 1)[-1]
+            if return_text and text:
+                yield text
+            elif return_raw:
+                yield raw
+            else:
+                tokens = process_text(text)
+                sentence = " ".join(tokens)
+                if not tokens or not sentence.strip():
+                    continue
+                yield sentence
 
-    if not exists:
-        for sentence in iter_sentences(errors):
-            res = cli.learn(x=sentence, model_name=model_name)
+    def kmeans(self, model_name="spack-errors", save_prefix="kmeans"):
+        """
+        Build the kmeans model with a particular name.
+        """
+        # Create the model if it does not exist (I created beforehand)
+        # Number clusters is kind of arbitrary, I wanted to do >> number of packages
+        exists = True
+        if model_name not in self.cli.models()["models"]:
+            model = feature_extraction.BagOfWords() | cluster.KMeans(
+                n_clusters=100, halflife=0.4, sigma=3, seed=0
+            )
+            model_name = self.cli.upload_model(model, "cluster", model_name=model_name)
+            exists = False
 
+        # Add each error to the server (only if not done yet)
+        if not exists:
+            for sentence in self.iter_sentences(self.errors):
+                res = self.cli.learn(x=sentence, model_name=model_name)
 
-def build_kmeans(cli, errors, model_name, datadir):
-    """
-    Build the kmeans model with a particular name.
-    """
-    # Create the model if it does not exist (I created beforehand)
-    # Number clusters is kind of arbitrary, I wanted to do >> number of packages
-    exists = True
-    if model_name not in cli.models()["models"]:
-        model = feature_extraction.BagOfWords() | cluster.KMeans(
-            n_clusters=100, halflife=0.4, sigma=3, seed=0
-        )
-        model_name = cli.upload_model(model, "cluster", model_name=model_name)
-        exists = False
+            # Save clusters to file under data/clusters/<prefix>
+            self.generate_clusters_json(model_name, save_prefix)
+        return self.load_model("%s.pkl" % model_name)
 
-    # Add each error to the server (only if not done yet)
-    if not exists:
-        for sentence in iter_sentences(errors):
-            res = cli.learn(x=sentence, model_name=model_name)
+    def dbstream(self, model_name="spack-dbstream-errors", save_prefix="dbstream"):
+        """
+        Build the dbstream model with a particular name.
+        https://riverml.xyz/latest/api/cluster/DBSTREAM
+        """
+        exists = True
+        if model_name not in self.cli.models()["models"]:
+            model = feature_extraction.BagOfWords() | cluster.DBSTREAM(
+                clustering_threshold=1.5,
+                fading_factor=0.05,
+                cleanup_interval=4,
+                intersection_factor=0.5,
+                minimum_weight=1,
+            )
+            model_name = self.cli.upload_model(model, "cluster", model_name=model_name)
+            exists = False
 
-    # At this point, let's get a prediction for each
-    # We can just group them based on the cluster
-    clusters = {}
-    for sentence in iter_sentences(errors):
-        res = cli.predict(x=sentence, model_name=model_name)
-        if res["prediction"] not in clusters:
-            clusters[res["prediction"]] = []
-        clusters[res["prediction"]].append(sentence)
+        if not exists:
+            for sentence in self.iter_sentences(self.errors):
+                res = self.cli.learn(x=sentence, model_name=model_name)
 
-    # Make model output directory
-    cluster_dir = os.path.join(datadir, "clusters", "kmeans")
-    if not os.path.exists(cluster_dir):
-        os.makedirs(cluster_dir)
+            # Save clusters to file under data/clusters/<prefix>
+            self.generate_clusters_json(model_name, save_prefix)
+            self.save_model(model_name)
+        return self.load_model("%s.pkl" % model_name)
 
-    for cluster_id, entries in clusters.items():
-        if not entries:
-            continue
-        cluster_meta = os.path.join(cluster_dir, "cluster-tokens-%s.json" % cluster_id)
-        write_json(entries, cluster_meta)
+    def denstream(self, model_name="spack-dbstream-errors", save_prefix="denstream"):
+        """
+        Build the denstream model https://riverml.xyz/latest/api/cluster/DenStream/
+        """
+        exists = True
+        if model_name not in self.cli.models()["models"]:
 
-    # Download the model and load to get centroids
-    cli.download_model(model_name=model_name)
-    with open("spack-errors.pkl", "rb") as fd:
-        model = pickle.load(fd)
+            # TODO the docs of denstream are not clear about beta/mu (and the range is wrong)
+            # and running like this we only get 2 clusters - I'm going to keep testing this.
+            model = feature_extraction.BagOfWords() | cluster.DenStream(
+                decaying_factor=0.01,
+                beta=1.01,
+                mu=1.0005,
+                epsilon=0.5,
+                n_samples_init=10,
+            )
+            model_name = self.cli.upload_model(model, "cluster", model_name=model_name)
+            exists = False
 
-    # Save the centers and create data for visualization because people expect it at this point :/
-    return model
+        if not exists:
+            for sentence in self.iter_sentences(self.errors):
+                res = self.cli.learn(x=sentence, model_name=model_name)
+
+            # Save clusters to file under data/clusters/<prefix>
+            self.generate_clusters_json(model_name, save_prefix)
+            self.save_model(model_name)
+        return self.load_model("%s.pkl" % model_name)
+
+    def generate_clusters_json(self, model_name, save_prefix):
+        """
+        Generate json cluster output for visual inspetion
+        """
+        # At this point, let's get a prediction for each
+        # We can just group them based on the cluster
+        clusters = {}
+        for sentence in self.iter_sentences(self.errors):
+            res = self.cli.predict(x=sentence, model_name=model_name)
+            if res["prediction"] not in clusters:
+                clusters[res["prediction"]] = []
+            clusters[res["prediction"]].append(sentence)
+
+        # Make model output directory
+        cluster_dir = os.path.join(self.datadir, "clusters", save_prefix)
+        if not os.path.exists(cluster_dir):
+            os.makedirs(cluster_dir)
+
+        for cluster_id, entries in clusters.items():
+            if not entries:
+                continue
+            cluster_meta = os.path.join(
+                cluster_dir, "cluster-tokens-%s.json" % cluster_id
+            )
+            write_json(entries, cluster_meta)
+
+    def save_model(self, model_name):
+        """
+        Save a pickled model (and return it)
+        """
+        # Download the model and load to get centroids
+        self.cli.download_model(model_name=model_name)
+
+    def load_model(self, pkl):
+        """
+        Load a model from pickle
+        """
+        with open(pkl, "rb") as fd:
+            model = pickle.load(fd)
+        return model
 
 
 def generate_embeddings(centers, name):
@@ -160,35 +227,23 @@ def main():
     # Build model with errors
     errors = read_errors(datadir)
 
-    # Make model output directory
-    model_dir = os.path.join(datadir, "models")
-    if not os.path.exists(model_dir):
-        os.makedirs(model_dir)
-
     if not os.path.exists("docs"):
         os.makedirs("docs")
 
-    # Create a river client (this is for development server, not yet spack monitor)
-    # cli = Client("http://127.0.0.1", prefix="ml")
-    cli = Client("http://127.0.0.1:8000")
+    builder = ModelBuilder(datadir=datadir, errors=errors)
 
     # Get models to see if we have spack-errors
-    # models = cli.models()
+    # models = builder.cli.models()
 
     # Build kmeans model and export clusters
     # Note that we don't need to keep doing that - spack-monitor can visualize them now
-    # model = build_kmeans(cli=cli, errors=errors, model_name="spack-errors", datadir=datadir)
+    # model = builder.kmeans(model_name="spack-errors")
 
-    # Save the centers and generate distance matrix
-    # centers = model.steps["KMeans"].centers
-    # write_json(centers, os.path.join(cluster_dir, "centers.json"))
+    # Note that I removed saving embeddings here since we expect to do this on
+    # spack monitor server.
 
-    # UIDS as id for each row, vectors across columns
-    # centers = list(centers.values())
-    # generate_embeddings(centers, "kmeans")
-    model = build_dbstream(
-        cli=cli, errors=errors, model_name="spack-dbstream-errors", datadir=datadir
-    )
+    # model = builder.dbstream(model_name="spack-dbstream-errors")
+    model = builder.denstream(model_name="spack-denstream-errors")
 
 
 if __name__ == "__main__":
